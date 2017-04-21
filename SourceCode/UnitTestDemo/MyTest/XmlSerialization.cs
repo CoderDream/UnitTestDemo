3b9d1029cfc6697bf757ca2b6f5fe77819a90621
CoderDream <coderdream@gmail.com>
2017-04-21 09:47:54 +0800

    add first version

---
+++ SourceCode/UnitTestDemo/MyTest/XmlSerialization.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTest
+{
+    class XmlSerialization
+    {
+        public bool WriteXml<T>(T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SourceCode/UnitTestDemo/MyTest/XmlSerializationTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+
+namespace MyTest
+{
+    [TestClass]
+    public class XmlSerializationTest
+    {
+        [TestMethod]
+        public void TestWriteXml()
+        {
+            UserModel user = new UserModel();
+            XmlSerialization serialization = new XmlSerialization();
+            bool flag = serialization.WriteXml<UserModel>(user);
+            Assert.IsTrue(flag);
+        }
+    }
+}
